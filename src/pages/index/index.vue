9228ce9b5fe1ee4c9a6e766311914b7079898fc9
李金芮 <jinrui.li@mishi.cn>
2019-08-07 18:09:01 +0800

    1.优化html结构； 2.更新部分依赖包；

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,39 +8,29 @@
       :interval="interval" 
       :duration="duration" 
       :circular="circular"
-      @change="swiperChange" 
-      :previous-margin="'58rpx'"
-      :next-margin="'58rpx'">
-      <div 
-        v-for="(bannerItem, bannerIndex) in bannerList" 
-        :key="bannerIndex">
-        <swiper-item>
-          <div 
+      :previous-margin="previousMargin"
+      :next-margin="nextMargin"
+      @change="swiperChange" >
+      <swiper-item v-for="(bannerItem, bannerIndex) in bannerList" :key="bannerIndex">
+        <!-- 效果实现关键点，在于class名为image-container的元素的style中justify-content与padding的值，该值在currentIndex为0和最后一张图的时候，与其他位置的值不太一样，需要自行设定 -->
+        <div class="image-container"
+          :style="{
+            justifyContent: currentIndex===0?((bannerIndex===bannerList.length-1)?'flex-end':(bannerIndex===1?'flex-start':'center')):(currentIndex===bannerList.length-1?(bannerIndex===0?'flex-start':(bannerIndex===bannerList.length-2?'flex-end':'center')):(bannerIndex===currentIndex-1?'flex-end':(bannerIndex===currentIndex+1?'flex-start':'center'))), 
+            padding: currentIndex===0?((bannerIndex===bannerList.length-1)?'56rpx 26rpx 0 0':(bannerIndex===1?'56rpx 0 0 26rpx':'56rpx 0 0 0')):(currentIndex===bannerList.length-1?(bannerIndex===0?'56rpx 0 0 26rpx':(bannerIndex===bannerList.length-2?'56rpx 26rpx 0 0':'56rpx 0 0 0')):(bannerIndex===currentIndex-1?'56rpx 26rpx 0 0':(bannerIndex===currentIndex+1?'56rpx 0 0 26rpx':'56rpx 0 0 0')))
+          }">
+          <!-- 正在展示的图片放大一定倍数 -->
+          <image class="slide-image" 
+            :src="bannerItem" 
             :style="{
-              boxSizing: 'border-box', 
-              width: '100%', 
-              height: '100%', 
-              display: 'flex', 
-              justifyContent: currentIndex===0?((bannerIndex===bannerList.length-1)?'flex-end':(bannerIndex===1?'flex-start':'center')):(currentIndex===bannerList.length-1?(bannerIndex===0?'flex-start':(bannerIndex===bannerList.length-2?'flex-end':'center')):(bannerIndex===currentIndex-1?'flex-end':(bannerIndex===currentIndex+1?'flex-start':'center'))), 
-              padding: currentIndex===0?((bannerIndex===bannerList.length-1)?'56rpx 26rpx 0 0':(bannerIndex===1?'56rpx 0 0 26rpx':'56rpx 0 0 0')):(currentIndex===bannerList.length-1?(bannerIndex===0?'56rpx 0 0 26rpx':(bannerIndex===bannerList.length-2?'56rpx 26rpx 0 0':'56rpx 0 0 0')):(bannerIndex===currentIndex-1?'56rpx 26rpx 0 0':(bannerIndex===currentIndex+1?'56rpx 0 0 26rpx':'56rpx 0 0 0')))
-            }">
-            <image :src="bannerItem" 
-              class="slide-image" 
-              :style="{
-                transform: currentIndex===bannerIndex?'scale(' + scaleX + ',' + scaleY + ')':'scale(1,1)',
-                transitionDuration: '.3s',
-                transitionTimingFunction: 'ease'
-              }" 
-              @click="getBannerDetail(bannerIndex)"/>
-          </div>
-        </swiper-item>
-      </div>
+              transform: currentIndex===bannerIndex?'scale(' + scaleX + ',' + scaleY + ')':'scale(1,1)'
+            }" 
+            @click="getBannerDetail(bannerIndex)"/>
+        </div>
+      </swiper-item>
     </swiper>
   </div>
 </template>
-
 <script>
-
 export default {
   data () {
     return {
@@ -51,10 +41,12 @@ export default {
       interval: 3000,
       duration: 300,
       circular: true,
+      previousMargin: '58rpx', // 前一张图片的外边距
+      nextMargin: '58rpx', // 后一张图片的外边距
       currentIndex: 0,
-      animationIndex: 0,
       scaleX: 1.1527,
       scaleY: 1.1524,
+      // 图片数组，需要至少3张图
       bannerList: [
         'https://mishi-image.oss-cn-hangzhou.aliyuncs.com/yry/wxapp/test/goodslist/menu-gulaorou.png',
         'https://mishi-image.oss-cn-hangzhou.aliyuncs.com/yry/wxapp/test/goodslist/menu-heniu.png',
@@ -84,10 +76,18 @@ export default {
   width: 100vw;
   height: 524rpx;
 }
+.image-container {
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
 .slide-image {
   width: 550rpx;
   height: 328rpx;
   z-index: 200;
   border-radius: 16rpx;
+  transition-duration: .3s;
+  transition-timing-function: 'ease';
 }
 </style>
